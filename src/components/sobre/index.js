fed5eadd986e607f9b78b0b312028fa4ee74cd0b
viniborges99 <borgesvinicius27@gmail.com>
2024-07-27 15:29:02 -0300

    scrollTop

--- src/components/sobre/index.js
+++ src/components/sobre/index.js
@@ -9,7 +9,7 @@ function Sobre() {
       <ContainerSecundario>
         <ContainerSobre>
           <h1> Uma paixão pelo churrasco!!</h1>
-          <P>Loucos por Tempero é uma empresa especializada em criar sabores únicos para o seu churrasco. Utilizamos técnicas inovadoras de mistura de sabores, empregando os melhores produtos do mercado, tudo feito artesanalmente.Nossa inspiração surgiu da paixão pelo sabor autêntico do churrasco brasileiro. Atuamos com inovação para diferenciar o seu churrasco do dia a dia.Além disso, somos especializados no desenvolvimento das melhores técnicas de preparo para o seu churrasco. Com uma equipe treinada, organizamos seu evento de forma sofisticada, seja no estilo brasileiro ou no American Barbecue.Oferecemos diversas opções, como Parrilla, fogo de chão e defumação. E para sua conveniência, nossos produtos e temperos já vêm inclusos no pacote.</P>
+          <P>Loucos por Tempero é uma empresa especializada em criar sabores únicos para o seu churrasco. Utilizamos técnicas inovadoras de mistura de sabores, empregando os melhores produtos do mercado, tudo feito artesanalmente.Nossa inspiração surgiu da paixão pelo sabor autêntico do churrasco brasileiro. Atuamos com inovação para diferenciar o seu churrasco do dia a dia.Além disso, somos especializados no desenvolvimento das melhores técnicas de preparo para o seu churrasco. Com uma equipe treinada, organizamos seu evento de forma sofisticada, seja no estilo brasileiro ou no American Barbecue. Oferecemos diversas opções, como Parrilla, fogo de chão e defumação. E para sua conveniência, nossos produtos e temperos já vêm inclusos no pacote.</P>
         </ContainerSobre>
         <ContainerImg>
           <img src={Carlos} alt="" />
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import{BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Evento from  '../components/pages/evento';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Evento from '../components/pages/evento';
 import Porcoes from '../components/pages/porçoes';
 import Temperos from '../components/pages/temperos';
 import Home from '../components/pages/Home';
+import ScrollTop from './scroll';
 
 
-function Rotas(){
-  return(
+function Rotas() {
+  return (
     <Router>
+      <ScrollTop />
       <Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<Home />} />
         <Route path="/Evento" element={<Evento />} />
         <Route path="/Porcoes" element={<Porcoes />} />
         <Route path="/Temperos" element={<Temperos />} />
